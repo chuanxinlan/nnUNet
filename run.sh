3adaab7117cf6f060608253a921251179b7172bf
chuanxinlan <chuanxin.lan@outlook.com>
2021-12-31 14:43:37 +0800

    *

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-iiiiiiiii
+export nnUNet_preprocessed="/home/cxlan/dataset"
+export RESULTS_FOLDER="/home/cxlan/nnUNet_trained_models"
+nnUNet_train 3d_fullres nnUNetTrainerV2 Task100_MALB 0 --npz
+#nnUNet_train 3d_fullres nnUNetTrainerV2 Task100_MALB ~/dataset/Task100_MALB --npz
